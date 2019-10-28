7bc609d835ec8bc8dff09bf493db870eb7fa2f5d
Manuel Guerrini <man.guerrini@gmail.com>
2019-10-28 16:33:53 +0100

    1 - trasformo la stringa in un array

--- 1/myscript.js
+++ 1/myscript.js
@@ -1,1 +1,8 @@
+// Una funzione per capire se la parola è palindroma
 
+// trasformo la stringa in un array
+var parola = prompt('scrivi una parola');
+console.log(parola);
+
+var parolaSplit = parola.split("");
+console.log(parolaSplit);
