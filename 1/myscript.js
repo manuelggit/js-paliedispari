1fbfcddbbf2ffbcabdabd1574dff53e97bbddc1c
Manuel Guerrini <man.guerrini@gmail.com>
2019-10-29 15:25:13 +0100

    1 - faccio un ciclo al contrario

--- 1/myscript.js
+++ 1/myscript.js
@@ -4,31 +4,26 @@
 var parola = prompt('scrivi una parola');
 console.log(parola);
 
-// var parolaSplit = parola.split("");
-// console.log(parolaSplit);
-//
-// // inverto l'array
-//
-// var arrayReverse = parolaSplit.reverse();
-// console.log(arrayReverse);
-//
-// // converto in stringa l'array invertito
-//
-// // var parolaReverse = arrayReverse.toString();
-// // console.log(parolaReverse);
-//
-// var rev = arrayReverse.join("");
-// console.log(rev);
+// ciclo for al contrario
 
-var rev = parola.split("").reverse("").join("");
-console.log(rev);
+var contraria = "";
 
-// stampo se è palindroma o no
-
-if(parola === rev){
-  console.log('palindroma')
-  document.getElementById('id').innerHTML = 'palindroma';
-}else{
-  console.log('non è palindroma');
-  document.getElementById('id').innerHTML = 'non è palindroma';
+for(var i = parola.length -1; i >= 0; i = i - 1) {
+  contraria = contraria + parola[i];
 }
+console.log(contraria);
+
+// con split.reverse.join
+
+// var rev = parola.split("").reverse("").join("");
+// console.log(rev);
+//
+// // stampo se è palindroma o no
+//
+// if(parola === rev){
+//   console.log('palindroma')
+//   document.getElementById('id').innerHTML = 'palindroma';
+// }else{
+//   console.log('non è palindroma');
+//   document.getElementById('id').innerHTML = 'non è palindroma';
+// }
